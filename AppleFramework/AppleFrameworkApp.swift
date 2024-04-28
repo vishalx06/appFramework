5f8ba104a0eaaf04d233ca2b3310751004c3df94
vishalx06 <vishalx06@gmail.com>
2024-04-28 20:50:23 +0530

    Initial Commit

---
+++ AppleFramework/AppleFrameworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworkApp.swift
+//  AppleFramework
+//
+//  Created by Vishal Sharma on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
